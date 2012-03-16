e281b03ff63bd4e76c00dbe198af6ca9c9b7256a
Mattia <info@matriz.it>
2012-03-16 19:29:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# jQuery UI Messenger
+
+
+
+## More info
+
+[jQuery UI Messenger - Matriz.it](http://www.matriz.it/projects/jquery-ui-messenger/ "Matriz | Projects | jQuery-UI-Messenger")
+
+## License
+
+This plugin is licensed under the MIT license.
+
+## Credits
+
+Mattia at [Matriz.it](http://www.matriz.it/) (info@matriz.it)
---
+++ jquery.ui.messenger.js
@@ -0,0 +1,11 @@
+/**
+* jQuery UI Messenger
+* @name jquery.ui.messenger.js
+* @author Mattia - http://www.matriz.it
+* @version 1.0.0
+* @date March 16, 2012
+* @category jQuery plugin
+* @copyright (c) 2012 Mattia at Matriz.it (info@matriz.it)
+* @license MIT - http://opensource.org/licenses/mit-license.php
+* @example Visit http://www.matriz.it/projects/jquery-ui-messenger/ for more informations about this jQuery plugin
+*/
